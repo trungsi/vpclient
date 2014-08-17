2688ddf8235de8264020c2c95828e8a56832faf4
ductrung.tran@gmail.com <ductrung.tran@gmail.com@e7057c96-90b2-6ef3-35eb-e9ac4b9ee6f3>
2014-08-17 20:15:35 +0000

    call addToCart service directly instead of using addToCart button or calling javascript

--- trunk/vpclient/src/test/java/com/trungsi/vpclient/VPClientTest.java
+++ trunk/vpclient/src/test/java/com/trungsi/vpclient/VPClientTest.java
@@ -19,6 +19,7 @@ import org.openqa.selenium.WebDriver;
 import static com.trungsi.vpclient.VPClient.*;
 import static com.trungsi.vpclient.utils.CollectionUtils.*;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author trungsi
@@ -60,12 +61,95 @@ public class VPClientTest extends AbstractVPClientTestCase {
 
     @Test
     public void testOpenXpressWindow() {
-        List<Map<String, String>> articles = findAllArticles("tecnica", "running");
+        List<Map<String, String>> articles = findAllArticles("kwala", "camper");
         //System.out.println(articles);
 
         openExpressPurchaseWindow(driver, articles.get(0));
         assertTrue(driver.findElements(By.xpath("//iframe[@id=\"viewer\"]")).size() > 0); // viewer in xpress window
+        //System.out.println(driver.getPageSource());
+    }
+
+    @Test
+    public void testGetFamilyAndProductId() {
+        List<Map<String, String>> articles = findAllArticles("kwala", "camper");
+        //System.out.println(articles);
+
+        openExpressPurchaseWindow(driver, articles.get(0));
         System.out.println(driver.getPageSource());
+
+        String familyId = getFamilyId(driver.getPageSource());
+        assertNotNull(familyId);
+        assertEquals(24, familyId.length());
+        assertTrue(familyId.endsWith("=="));
+    }
+
+    @Test
+    public void testGetSelectedProductId() {
+        String selectedMark = "kwala";
+        String selectedCategory = "camper";
+
+        List<Map<String, String>> saleList = getSalesList(driver);
+        System.out.println(saleList);
+
+        Map<String, String> selectedSale = getSelectedSale(saleList, selectedMark);
+        System.out.println(selectedSale);
+
+        context.put(SELECTED_SALE_DATE, selectedSale.get("dateSales"));
+        context.put(SELECTED_SALE_LINK, selectedSale.get("link"));
+
+        List<Map<String, String>> categories = findAllCategories(driver, context);
+        //System.out.println(categories);
+        Map<String, String> category = getSelectedCategory(categories, selectedCategory);
+
+        List<Map<String, String>> subCategories = findSubCategories(driver, category, context);
+        System.out.println(subCategories);
+
+        Map<String, String> subCategory = subCategories.get(0);
+
+        List<Map<String, String>> articles = findAllArticlesInSubCategory(driver, category, subCategory, context);
+        Map<String, String> article = articles.get(0);
+
+        openExpressPurchaseWindow(driver, article);
+
+        selectSize(driver, category, subCategory, article, context, "toto");
+
+        System.out.println(driver.getPageSource());
+
+        String selectedProductId = getSelectedProductId(driver.getPageSource());
+        assertNotNull(selectedProductId);
+        assertEquals(24, selectedProductId.length());
+        assertTrue(selectedProductId.endsWith("=="));
+
+        //addArticle(driver, category, subCategory, articles.get(0), context);
+    }
+
+    @Test
+    public void testAddArticle() {
+        String selectedMark = "kwala";
+        String selectedCategory = "camper";
+
+        List<Map<String, String>> saleList = getSalesList(driver);
+        System.out.println(saleList);
+
+        Map<String, String> selectedSale = getSelectedSale(saleList, selectedMark);
+        System.out.println(selectedSale);
+
+        context.put(SELECTED_SALE_DATE, selectedSale.get("dateSales"));
+        context.put(SELECTED_SALE_LINK, selectedSale.get("link"));
+
+        List<Map<String, String>> categories = findAllCategories(driver, context);
+        //System.out.println(categories);
+        Map<String, String> category = getSelectedCategory(categories, selectedCategory);
+
+        List<Map<String, String>> subCategories = findSubCategories(driver, category, context);
+        System.out.println(subCategories);
+
+        Map<String, String> subCategory = subCategories.get(0);
+
+        List<Map<String, String>> articles = findAllArticlesInSubCategory(driver, category, subCategory, context);
+        Map<String, String> article = articles.get(0);
+
+        addArticle(driver, category, subCategory, article, context);
     }
 
 
